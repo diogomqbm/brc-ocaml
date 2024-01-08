1e21a64f2cfd029f0fdd70386b8fe41e9902f57b
Diogo Mafra <diogo@mafra.sh>
2024-01-07 21:17:32 -0300

    wip: need to add limit

--- lib/files.ml
+++ lib/files.ml
@@ -2,53 +2,43 @@ open Riot.IO
 
 [@@@warning "-8"]
 
-let read_line reader buf_size =
-  let str = "" in
-  let rec _read_loop str =
-    let buf_reader = Reader.Buffered.of_reader reader ~capacity:buf_size in
-    let buf = Buffer.with_capacity 1 in
-    let (Ok bytes_read) = Reader.read ~buf buf_reader in
-    if Int.equal bytes_read 0 then None
-    else
-      let as_str = Buffer.to_string buf in
-      if String.equal as_str "\n" then Some str else _read_loop (str ^ as_str)
+let read_lines ~filename ~offset () =
+  let file = Riot.File.open_read filename in
+  let _ = Riot.File.seek file offset Unix.SEEK_SET in
+  let reader =
+    Riot.File.to_reader file |> Reader.Buffered.of_reader ~capacity:(1_024 * 100)
   in
-  _read_loop str
 
-let rec read_loop reader buf_size =
-  match read_line reader buf_size with
-  | Some line ->
-      Riot.Logger.debug (fun f -> f "Processing line %s" line);
-      let _pid =
-        Riot.spawn (fun () ->
-            match Parsed.from_line line with
-            | Some l -> Hashmap.add l
-            | None -> Riot.Logger.debug (fun f -> f "Skipping line"))
-      in
-      ();
-      read_loop reader buf_size
-  | None -> Riot.Logger.info (fun f -> f "Reached EOF")
+  let buf = Buffer.with_capacity 1 in
+  let[@warning "-8"] rec read_line line =
+    let (Ok bytes) = Reader.read ~buf reader in
+    if bytes = 0 then line
+    else
+      let data = Buffer.to_string buf in
+      let is_line = String.equal data "\n" in
+      let line = if is_line then line else line ^ data in
+
+      if is_line then Riot.Logger.info (fun f -> f "line: %S" line);
+
+      if is_line then
+        Option.iter (fun p -> Hashmap.add p) (Parsed.from_line line);
+
+      if is_line then read_line "" else read_line line
+  in
+  let _ = read_line "" in
+  ()
 
 let read_file filename =
   let chunk_size = 1000 in
-  let file_size =
-    LargeFile.in_channel_length (open_in filename) |> Int64.to_int
+  let state = Riot.File.stat filename in
+  let size = state.st_size in
+  let chunks = size / chunk_size in
+  let pids =
+    List.init chunks (fun chunk ->
+        let offset = chunk * chunk_size in
+        Riot.spawn (read_lines ~filename ~offset))
   in
-  let amount_of_chunks = Int.div file_size chunk_size in
-  print_endline (string_of_int file_size);
-  let rec loop n =
-    if n > amount_of_chunks then ()
-    else
-      let _pid =
-        Riot.spawn (fun () ->
-            let seek = Int.mul n chunk_size in
-            let file = Riot.File.open_read filename in
-            let _f = Riot.File.seek file seek Unix.SEEK_SET in
-            Riot.Logger.debug (fun f -> f "Reading file from %d" seek);
-            let reader = Riot.File.to_reader file in
-            read_loop reader chunk_size)
-      in
-      ();
-      loop (n + 1)
-  in
-  loop 0
+
+  Riot.wait_pids pids;
+  Riot.Logger.info (fun f -> f "done");
+  Riot.sleep 0.2
